b57285c5a10d2f42513b367692708c6af25c3c24
呆萌 <admin@diemoe.net>
2021-12-02 05:31:31 +0800

    Update mog_station_cn.user.js

--- mog_station_cn.user.js
+++ mog_station_cn.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         莫古station汉化
 // @namespace    http://touhou.diemoe.net/
-// @version      0.5
+// @version      0.6
 // @description  莫古station界面的简单中文化
 // @author       呆萌
 // @match        *://secure.square-enix.com/*
@@ -19,11 +19,15 @@
     menu2cn("menu_support_button","用户支持")
     menu2cn("logout_button","注销登录")
     id2cn("to_service_contract_button2","'最终幻想XIV'的服务合同程序，续约、注销、购入。");
-    id2cn("service_contract_button","'使用信用卡或水晶点购入月卡。");
+    id2cn("service_contract_button","使用信用卡或水晶点购入月卡。");
     id2cn("retainer_contract_button","追加购买雇员")
     id2cn("caoption_contract_button","助手APP增值服务")
     id2cn("charge_button","使用卡密兑换。");
     id2cn("code_input_button","兑换代码")
+    id2cn("early_code_input_button","兑换预约特典奖励代码")
+    id2cn("add_ps4_button","购入PS4/PS5版本")
+    id2cn("add_mac_button","不要买MAC版本，用这份钱给自己吃顿好的不行吗，就是个虚拟机Windows版")
+    id2cn("upgrade_ce_button","升级为数字典藏版");
 
     href2cn("/account/app/svc/member","账户信息");
     href2cn("/account/app/svc/mbrInfoList","账户资料修改");
@@ -43,10 +47,11 @@
     text2cn("プレイするプラットフォームの追加","在其他平台上游玩需要先在这里进行购买。")
     text2cn("「ファイナルファンタジーXIV」のサービス契約手続きを行います。","办理“最终幻想XIV”的服务合同手续。");
     text2cn("複数のサービスアカウントをお持ちの方は、まずお手続きしたいサービスアカウントを下記のタブメニューより選択してください。","有多个服务账号的人，请先从下面的标签菜单中选择想要办理的服务账号。");
+    text2cn("仮想通貨Crystaはドコモ払い、auかんたん決済、BitCach、Webマネーなど様々な支払い手段でチャージが行えます。詳しくは","Crysta 水晶点可以通过多种手段充值。详细情况");
     text2cn("サービスアカウントの選択","服务账号的选择")
     text2cn("アカウント種別","账户类型")
     text2cn("コンパニオンアプリ","助手APP")
-    text2cn("アプリを利用すると「お気に入りエーテライト」の登録可能数が増えます。詳しくは","使用应用程序的话，可以登录“收藏的王牌”的数量会增加。详细情况")
+    text2cn("アプリを利用すると「お気に入りエーテライト」の登録可能数が増えます。詳しくは","使用应用程序可以增加市场告示板“收藏夹”的数量上限。详细情况")
     text2cn("バージョン","资料片")
     text2cn("プレイ権があるプラットフォームに”○”が表示されます。","使用‘○’表示已获得游玩权限的平台")
     text2cn("サービス契約状態","月卡状态")
@@ -55,7 +60,9 @@
     text2cn("契約日数を追加する","现在充值月卡")
     text2cn("キャラクターリスト","角色列表")
     text2cn("デジタルアップグレードサービス","升级为典藏版 / 获取数字特典")
+    id2cn("digital_upgrade_button_txt","升级为典藏版");
     text2cn("ホームワールド変更サービス","角色数据转移服务")
+    id2cn("world_transfer_button_txt","转移所属服务器");
     text2cn("キャラクター名変更サービス","角色名称修改服务")
     text2cn("ベテランリワード","陆行鸟礼物站(x)")
 
@@ -72,6 +79,8 @@
     text2cn("蒼天のイシュガルド","苍穹之禁城")
     text2cn("紅蓮のリベレーター","红莲之狂潮")
     text2cn("漆黒のヴィランズ","暗影之逆焰")
+    text2cn("暁月のフィナーレ","晓月之终途")
+    text2cn("アーリーアクセス","抢先体验权")
     text2cn("サービスコース","服务套餐")
     text2cn("累積利用日数","累计使用天数")
     text2cn("お支払い方法","支付方式")
@@ -106,12 +115,28 @@
     text2cn("（再入力）","（重复一次）");
     text2cn("パスワードの変更","密码修改");
     text2cn("メールアドレスの変更","修改邮箱");
-    text2cn("アカウントの登録解除","注销（删号）");
+    text2cn("アカウントの登録解除","注销账号（删号）");
     text2cn(/スクウェア・エニックス/g,"SQUARE·ENIX");
 
     function text2cn(text,cn){
-        var html= $("#base_body").html();
-        $("#base_body").html(html.replace(text,cn));
+        try {
+            var base_body_html= $("#base_body").html();
+            $("#base_body").html(base_body_html.replace(text,cn));
+        }
+        catch(err) {
+        }
+        try {
+            var bound_out_html= $(".bound_out").html();
+            $(".bound_out").html(bound_out_html.replace(text,cn));
+        }
+        catch(err) {
+        }
+        try {
+            var wrapBody_html= $("#wrapBody").html();
+            $("#wrapBody").html(wrapBody_html.replace(text,cn));
+        }
+        catch(err) {
+        }
     }
     function id2cn(id,cn){
         $("#"+id).html(cn);
